8f0cef4fdeb91fa048aaf3aba5a1c3813f1d32bb
Artem <richhaho@gmail.com>
2023-11-23 16:44:44 -0500

    team info

---
+++ team.php
@@ -0,0 +1,17 @@
+- Hyon
+skype: lionheartking12345678@hotmail.com
+starcraft0429@hotmail.com
+
+- Kim Unchol
+orion763357@gmail.com
+ab418bear@hotmail.com(skype: FedorOrion)
+
+- Jong
+live:gold.god313
+happyboy3294@gmail.com
+
+- Ri SR
+live:.cid.a0f2f3a6932d8c20
+goldbright00000@gmail.com
+
+
