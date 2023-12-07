2b213eff742b8189d10cf2e2ba0238f458eb17da
richhaho <73793793+richhaho@users.noreply.github.com>
2023-12-07 12:08:11 -0500

    Update team.php

--- team.php
+++ team.php
@@ -21,3 +21,75 @@ Kim cholbong: 133 3163 2818
 HR river: 13039081979  15944321829
 Ship Factory: 17643382001
 RG SB: 13844392617
+
+
+
+
+[
+    {
+        "id": "HMS",
+        "email": "starcraft0429@hotmail.com",
+        "phone1": "13641143389",
+        "phone2": "16503263678",
+        "skp": "lionheartking12345678@hotmail.com"
+    },
+    {
+        "id": "KUCH",
+        "email": "orion763357@gmail.com",
+        "phone1": "",
+        "phone2": "",
+        "skp": "ab418bear@hotmail.com(id: FedorOrion)"
+    },
+    {
+        "id": "JGS",
+        "email": "happyboy3294@gmail.com",
+        "phone1": "",
+        "phone2": "",
+        "skp": "live:gold.god313"
+    },
+    {
+        "id": "RSR",
+        "email": "goldbright00000@gmail.com",
+        "phone1": "",
+        "phone2": "",
+        "skp": "live:.cid.a0f2f3a6932d8c20"
+    },
+    {
+        "id": "KGSNG",
+        "email": "beautistar1112@gmail.com",
+        "phone1": "",
+        "phone2": "",
+        "skp": "live:lowell1986713"
+    },
+    {
+        "id": "FOX",
+        "email": "li.starlight05@gmail.com",
+        "phone1": "",
+        "phone2": "",
+        "skp": "live:angela.leong_1"
+    },
+    {
+        "id": "Welding ltd",
+        "email": "designerzedd@gmail.com",
+        "phone1": "",
+        "phone2": "",
+        "skp": "live:.cid.5c82f288e8c02ec7"
+    },
+    {
+        "id": "Brokers",
+        "Payoneer RomanKit": "live:romchos",
+        "Didenko": {
+            "phone": "+380958615089",
+            "email": "themrdidenko@gmail.com",
+            "skype": "themrdidenko"
+        },
+        "Binance": "live:.cid.2b8640b3eea613ec"
+    },
+    {
+        "id": "CHN",
+        "RCHO": "15568006533, 15568008985  Lizhe716@hotmail.co.jp",
+        "KCHB": "13331632818",
+        "HR_KMS": "13039081979",
+        "SHIP_FACT": "13844392617"
+    }
+]
